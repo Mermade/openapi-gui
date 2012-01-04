6deded7b9872b6a630de5651e0833cbd9c573967
Brandon West <brawest@gmail.com>
2012-01-03 17:01:16 -0700

    temporarily rename layout.haml, add code to JSON.parse uploaded files

--- index.rb
+++ index.rb
@@ -1,12 +1,26 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'json'
 
 get '/' do
   haml :index, :format => :html5
 end
 
 post '/:config' do
-  
   @parsed = JSON.parse()
 end
+
+post '/' do
+   unless params[:file] &&
+          (tmpfile = params[:file][:tempfile]) &&
+          (name = params[:file][:filename])
+     @error = "No file selected"
+     return haml(:index)
+   end
+   STDERR.puts "Uploading file, original name #{name.inspect}"
+   contents = tmpfile.read
+   parsed = JSON.parse(contents)
+     
+   "Upload complete #{parsed.inspect}"
+ end
