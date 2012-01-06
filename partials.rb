9f9a6bf3692af2509e28f99d1f56b05c7d204017
Brandon West <brawest@gmail.com>
2012-01-06 12:54:46 -0700

    add partials.rb for more robust partial view support

---
+++ partials.rb
@@ -0,0 +1,18 @@
+#from https://gist.github.com/119874
+module Sinatra::Partials
+  def partial(template, *args)
+    template_array = template.to_s.split('/')
+    template = template_array[0..-2].join('/') + "/_#{template_array[-1]}"
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    options.merge!(:layout => false)
+    locals = options[:locals] || {}
+    if collection = options.delete(:collection) then
+      collection.inject([]) do |buffer, member|
+        buffer << haml(:"#{template}", options.merge(:layout =>
+        false, :locals => {template_array[-1].to_sym => member}.merge(locals)))
+      end.join("\n")
+    else
+      haml(:"#{template}", options)
+    end
+  end
+end
