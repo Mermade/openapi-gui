b3c810503fa57f9ca5eab40cc4978fda86cb3e66
Brandon West <brawest@gmail.com>
2012-01-10 09:51:17 -0700

    add some comments to js

--- public/js/default.js
+++ public/js/default.js
@@ -4,12 +4,14 @@ $(function() {
 		var id = actuator.id.replace("endpointActuator","");
 		var div = $('#ep' + id);
 		
+		//if the clicked div is already showing, do nothing
 		if(div.is(":visible"))
 			return;
 		
+		//hide all divs
 		$('.endpoint').hide();
 		
-		//show the div
+		//show the div corresponding to what was clicked
 		$('#ep' + id).show();
 	})
 });
