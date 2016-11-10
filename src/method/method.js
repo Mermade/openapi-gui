d2d2d96ba89391c6033b53a48697d5d174664888
Mike Ralphson <mike.ralphson@gmail.com>
2016-11-10 07:49:52 +0000

    Add parameter

--- src/method/method.js
+++ src/method/method.js
@@ -13,6 +13,14 @@ Vue.component('api-method', {
 		removeOperation : function(target) {
             this.$parent.removeOperation(target);
 	    },
+        addParameter : function() {
+            var newParam = {};
+            newParam.name = 'newParam';
+            newParam.in = 'query';
+            newParam.required = 'true';
+            newParam.type = 'string';
+            this.method.parameters.push(newParam);
+        },
     },
     computed: {
         httpMethod : function() {
