7ba5a48a3123912b60178b51ea9a91359ee1e4ca
Mike Ralphson <mike.ralphson@gmail.com>
2017-04-22 13:13:59 +0100

    Make adding operation+response use default not 200

--- src/resource/resource.js
+++ src/resource/resource.js
@@ -43,8 +43,8 @@ Vue.component('api-resource', {
             }
             if (index<this.methods.length) {
                 var responses = {};
-                responses["200"] = {
-                    description: "default"
+                responses.default = {
+                    description: "Default response"
                 };
                 var op = {};
                 op.summary = template && template.summary || '';
